5d1eb745e8be5f583024d4a95b7b791899728415
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-02 09:53:25 -0700

    skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Skills from './components/Skills';
+import Projects from './components/Projects'
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
